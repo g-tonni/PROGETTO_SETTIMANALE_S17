aded9f2c6bbcd3d21f1ca0a20faffe5b0069f04b
Giada <tonni.giada.210802@gmail.com>
2026-02-06 09:35:49 +0100

    Initial commit

---
+++ src/main/java/giadatonni/PROGETTO_SETTIMANALE_S17/ProgettoSettimanaleS17Application.java
@@ -0,0 +1,13 @@
+package giadatonni.PROGETTO_SETTIMANALE_S17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleS17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleS17Application.class, args);
+	}
+
+}
---
+++ src/test/java/giadatonni/PROGETTO_SETTIMANALE_S17/ProgettoSettimanaleS17ApplicationTests.java
@@ -0,0 +1,13 @@
+package giadatonni.PROGETTO_SETTIMANALE_S17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleS17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
